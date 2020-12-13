67909e1a6945aa79bee7bee81254dfe78405d8d9
TonikX <antongovorov@gmail.com>
2020-12-13 19:01:33 +0300

    new lesi with task with teacher

--- application/courses_app/views.py
+++ application/courses_app/views.py
@@ -729,9 +729,9 @@ class TaskWithTeacherCheckResultForTeacherListView(generics.ListAPIView):
         Вывод всех результатов для одной рабочей программы по id
         """
         # Note the use of `get_queryset()` instead of `self.queryset`
-        course = StudentStream.objects.filter(id = self.kwargs['stream_id'])[0].course_access
-        print(course)
-        queryset = TaskWithTeacherCheckResult.objects.filter(option__task__section__course = course)
+        course = StudentStream.objects.filter(id = self.kwargs['stream_id'])[0].course_access.id
+        print('d',course)
+        queryset = TaskWithTeacherCheckResult.objects.filter(option__task__section__course__id = course)
         serializer = TaskWithTeacherCheckResultSerializer(queryset, many=True)
         return Response(serializer.data)
 
